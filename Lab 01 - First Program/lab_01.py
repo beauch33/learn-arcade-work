9003800951db6d9ac0fefd6f1c2f0fb7ac7c933d
beauch33 <beauch33@gmail.com>
2024-06-25 13:51:57 -0700

    Work on lab 1

---
+++ Lab 01 - First Program/lab_01.py
@@ -0,0 +1,15 @@
+print("\"Hello lab 1 assignment!\" said the person writing this code.")
+print("\n")
+
+print("We can type\non multiple\nlines\nby entering in \"\\n\".")
+print("\n")
+
+print("""Or we can type
+on multiple
+lines
+by entering in triple quotes.""")
+
+print("\n")
+print("Or for better readability")
+print("We should be using")
+print("multiple print statements.")
